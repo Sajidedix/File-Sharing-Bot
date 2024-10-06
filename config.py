cb5fc0830d81be0c81f6fb18a4a653a9a3ecdd03
Sajidedix <sajideditz27@gmail.com>
2024-10-06 19:50:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,8 +58,7 @@ PICS = (environ.get('PICS', 'https://i.ibb.co/WPTP42j/image.jpg')).split()
 
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel
-And Other Users Can Access It From Special Link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 
